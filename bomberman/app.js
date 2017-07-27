1d272d894363a4fcb252bf83840795533e0a2609
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-27 10:13:18 -0700

    Bomber controls

--- bomberman/app.js
+++ bomberman/app.js
@@ -2,6 +2,10 @@ class DispalyObject {
   constructor(props) {
     Object.assign(this, props)
   }
+  update(){
+    this.x = this.x+this.vx
+    this.y = this.y+this.vy
+  }
   render(){
     image(this.pic, this.x, this.y)
   }
@@ -17,11 +21,30 @@ function preload() {
 function setup() {
   createCanvas(400, 300)
   bguyPic.resize(50, 50)
-  guy = new DispalyObject({x: 0, y: 0, pic: bguyPic})
+  guy = new DispalyObject({x: 0, y: 0, pic: bguyPic, vx: 0, vy: 0})
 }
 
 function draw() {
   background('pink')
+  guy.update()
   guy.render()
-  // image(bguyPic, guyX, guyY)//image, x, y
+}
+
+function keyPressed() {
+  if (keyCode===RIGHT_ARROW) {
+    guy.vx=1
+  }
+  if (keyCode===LEFT_ARROW) {
+    guy.vx=-1
+  }
+  if (keyCode===UP_ARROW) {
+    guy.vy=1
+  }
+  if (keyCode===DOWN_ARROW) {
+    guy.vy=-1
+  }
+}
+function keyReleased() {
+  guy.vx=0
+  guy.vy=0
 }
